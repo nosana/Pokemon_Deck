b79de0f6541892216a709dbae3c894b14aec0c32
nosana <novanopan26@gmail.com>
2022-11-15 12:16:44 +0700

    second

--- index.js
+++ index.js
@@ -1,112 +1,4 @@
 
-//array
-let array=[1,2,3,4];
-console.log(array);
-//insert array last
-array.push("halo");
-console.log(array);
-array.pop();
-console.log(array);
-//insert depan
-array.unshift("testing");
-console.log(array);
-array.shift();
-console.log(array);
-
-//array multidimensional
-let arrayMulti = ['a','b',3,[4,5,'d'],true];
-console.log(arrayMulti[3][2]);
-
-//arrow function
-let hitung= (a,b)=>a+b;
-console.log(hitung(5,10));
-//anonymous function
-let hitungAnon= function(x,y){
-    return x+y;
-}
-console.log(hitungAnon(10,15));
-
-
-const nilai = 50;
-console.log(nilai);
-
-//object
-const mhs={
-    nim:"a112000",
-    name:"budi",
-    age:50,
-    hobby:['mancing','tawuran','ngoding'],
-    isActive:true
-}
-
-const user = {};
-user.username = "budi";
-user.password = "1234";
-
-console.log(mhs.hobby[2]);
-
-// array of object
-
-const animals = [
-    {name:"budi",species:"dog",class:{name:"mamalia"}},
-    {name:"nemo",species:"fish",class:{name:"invertebrata"}},
-    {name:"bayu",species:"dog",class:{name:"mamalia"}},
-    {name:"jalin",species:"dog",class:{name:"mamalia"}},
-    {name:"dory",species:"fish",class:{name:"invertebrata"}}
-]
-
-//buat sebuah fungsi untuk mengambil object yg speciesnya dog,
-//lalu dimasukkan ke dalam variabel OnlyDog
-
-//jika fish ubah class menjadi ovipar
-let OnlyDog = [];
-
-animals.forEach(animal => {
-    if(animal.species =="dog"){
-        OnlyDog.push(animal) ;
-    }
-});
-
-let onlyDogFilter = animals.filter(animal=>animal.species=="dog");
-let isDog = animals.map(animal=>{
-    return {
-        name : animal.name,
-        isDog : animal.species == "dog" ? true:false
-    }
-})
-console.log(isDog);
-
-// let judul = document.getElementById("judul");
-// let paragrafAja = document.getElementsByTagName("p")[0];
-// paragrafAja.style.backgroundColor = "blue";
-
-// let paragrafAja2 = document.querySelector(".list:nth-child(3)")
-
-
-// judul.onclick = function(){
-//     judul.innerHTML = "berubah";
-// }
-// judul.onclick = function(){
-//     judul.style.backgroundColor = "red";
-// }
-
-// function berubah(){
-//     console.log("tes");
-//     let judul = document.getElementById("judul");
-//     judul.style.backgroundColor = "red";
-//     judul.innerHTML = "beru\bah";
-// }
-
-// judul.addEventListener('click',function(){
-//     judul.innerHTML = "masuk";
-// });
-// judul.addEventListener('click',function(){
-//     judul.style.backgroundColor = "red";
-// });
-
-// $(".list:nth-child(3)").on('click',function(){
-//     $(".list:nth-child(3)").html("ini diubah dari Jquery");
-// });
 
 $.ajax({
     url: "https://pokeapi.co/api/v2/pokemon/", 
@@ -138,8 +30,12 @@ function detailPoke(url){
         url: url
     }).done((res)=>{
         console.log(res);
+        
         $(".modal-title").html(res.name);
         $("#gambarPoke").attr('src',res.sprites.other.dream_world.front_default)
+
+       
+
 
 
         let typePoke = ""
@@ -147,7 +43,7 @@ function detailPoke(url){
             // literal template
            
              typePoke += `
-             <span class="badge rounded-pill bg-warning text-dark">${val.type.name}</span> 
+             <span class="badge rounded-pill text-capitalize ${val.type.name}">${val.type.name}</span> 
                     `
             
         })
@@ -167,34 +63,78 @@ function detailPoke(url){
 
    
        
-        
+        let progressbarStats="";
+        $.each(res.stats,function(key,val){
+            let statPercentage = Math.round((val.base_stat/200)*100);
+            progressbarStats += 
+                `<p class="statNames">${val.stat.name}</p>
+                <div class="progress" style="height: 30px;">
+                    <div class="progress-bar progress-bar-striped bg-success ${val.stat.name}" role="progressbar" style="width: ${statPercentage}%;" aria-valuenow="${val.base_stat}" aria-valuemin="0" aria-valuemax="255">${val.base_stat}</div>
+                </div>`;        
+        })
+        $("#stats").html(progressbarStats);
 
        
        
-        $("#gantiPoke").text(res.name)
-        $("#hp").html(res.stats[0].stat.name)
-        $('#Php').html(res.stats[0].base_stat)
+        $("#gantiPoke").text(res.name.toUpperCase())
+        // $("#hp").html(res.stats[0].stat.name)
+        // $('#Php').html(res.stats[0].base_stat)
 
-        $("#attack").text(res.stats[1].stat.name)
-        $('#PAttack').html(res.stats[1].base_stat)
+        // $("#attack").text(res.stats[1].stat.name)
+        // $('#PAttack').html(res.stats[1].base_stat)
 
-        $("#defense").text(res.stats[2].stat.name)
-        $("#PDefense").text(res.stats[2].base_stat)
+        // $("#defense").text(res.stats[2].stat.name)
+        // $("#PDefense").text(res.stats[2].base_stat)
 
-        $("#special-attack").text(res.stats[3].stat.name)
-        $("#Psa").text(res.stats[3].base_stat)
+        // $("#special-attack").text(res.stats[3].stat.name)
+        // $("#Psa").text(res.stats[3].base_stat)
 
-        $("#special-defense").text(res.stats[4].stat.name)
-        $("#Psd").text(res.stats[4].base_stat)
-
-        
-
+        // $("#special-defense").text(res.stats[4].stat.name)
+        // $("#Psd").text(res.stats[4].base_stat)
 
         
-         $("#Weight").html(res.weight)
+        let weightPoke= "";
+        weightPoke = `
+        <h4>Weight</h4>
+        <div>${res.weight} kg</div>
+        `
+        $("#Weight").html(weightPoke);
+
+        let heightPoke= "";
+        heightPoke = `
+        <h4>Height</h4>
+        <div>0.${res.height} m</div>
+        `
+        $("#Height").html(heightPoke);
+
+
+        
+        //  $("#Weight").html(res.weight)
      
-        $("#Height").html(res.height)
-       
+        // $("#Height").html(res.height)
+       poke_species_details(res.species.url);
+
+        function poke_species_details(url){
+            $.ajax({
+              url:url
+            }).done((res)=>{
+              console.log(res.flavor_text_entries[37].flavor_text);
+              let pokemon_desc = "";
+              pokemon_desc = `${res.flavor_text_entries[37].flavor_text}`
+              $("#pokemon-desc").html(pokemon_desc);
+            })
+          }
+
+          mencoba(res.species.url);
+
+        function mencoba(url){
+            $.ajax({
+              url:url
+            }).done((res)=>{
+              console.log(res.flavor_text_entries);
+             
+            })
+          }
 
     });
 
